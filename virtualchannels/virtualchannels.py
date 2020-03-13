e0b726b411e53a5a9fbbfdc356d96ce4f24798ca
DJ Seeds <dseeds14@gmail.com>
2020-03-12 17:42:42 -0700

    Add required hooks and initial message types

--- virtualchannels/virtualchannels.py
+++ virtualchannels/virtualchannels.py
@@ -1,31 +1,105 @@
 #!/usr/bin/env python3
+from enum import Enum
 from pyln.client import Plugin
 
+class MessageType(Enum):
+  PROPOSE_VIRTUAL_RECEIVE= 0xFFA9
+  ACCEPT_VIRTUAL_RECEIVE= 0xFFAB
+  FAIL_VIRTUAL_RECEIVE= 0xFFAD
+  PROPOSE_VIRTUAL_SEND= 0xFFAF
+  VIRTUAL_SEND_COMPLETE= 0xFFB1
+  FAIL_VIRTUAL_SEND= 0xFFB3
+
 plugin = Plugin()
-
-@plugin.method("hello")
-def hello(plugin, name="world"):
-    """This is the documentation string for the hello-function.
-
-    It gets reported as the description when registering the function
-    as a method with `lightningd`.
-
-    """
-    greeting = plugin.get_option('greeting')
-    s = '{} {}'.format(greeting, name)
-    plugin.log(s)
-    return s
-
 
 @plugin.init()
 def init(options, configuration, plugin):
     plugin.log("Plugin helloworld.py initialized")
 
 
-@plugin.subscribe("connect")
-def on_connect(plugin, id, address):
-    plugin.log("Received connect event for peer {}".format(id))
+@plugin.hook("htlc_accepted")
+def on_htlc_connected(plugin, onion, next_onion, shared_secret, htlc):
+  """ Main method for receiving on behalf of trusted node.
+  """
+  plugin.log("Got an incoming HTLC htlc={}, onion={}".format(htlc, onion))
+  return {"result": "continue"}
 
+
+@plugin.hook("rpc_command")
+def on_rpc_command(plugin, rpc_command):
+  """ Routes RPC commands to handlers or allows clightning to continue.
+  """
+  plugin.log("Got an incoming RPC command method={}".format(rpc_command.method))
+  handlers = {
+    'sendpay': on_sendpay,
+    'invoice': on_invoice,
+  }
+
+  handler = handlers.get(rpc_command.method, lambda: {"result": "continue"})
+  return handler(plugin, rpc_command)
+
+
+def on_sendpay(plugin, rpc_command):
+  return {"result": "continue"}
+
+
+def on_invoice(plugin, rpc_command):
+  return {"result": "continue"}
+
+@plugin.hook("custommsg")
+def on_custommsg(plugin, peer_id, message):
+  # Split message into type and contents
+  # Dispatch to handler if one exists
+  (type, value) = parse_message(message)
+  handlers = {
+    MessageType.PROPOSE_VIRTUAL_RECEIVE: on_propose_virtual_receive,
+    MessageType.ACCEPT_VIRTUAL_RECEIVE: on_accept_virtual_receive,
+    MessageType.FAIL_VIRTUAL_RECEIVE: on_fail_virtual_receive,
+    MessageType.PROPOSE_VIRTUAL_SEND: on_propose_virtual_send,
+    MessageType.VIRTUAL_SEND_COMPLETE: on_virtual_send_complete,
+    MessageType.FAIL_VIRTUAL_SEND: on_fail_virtual_send,
+  }
+
+  handler = handlers.get(type, lambda: None)
+  handler(value)
+  return {"result": "continue"}
+
+def parse_message(message):
+  try:
+    type = int(message[0:4], 16)
+    return (type, message[4:])
+  except ValueError:
+    return (None, None)
+
+def on_propose_virtual_receive(message):
+  """ Contents: payment_hash amount
+  """
+  pass
+
+def on_accept_virtual_receive(message):
+  """ Contents: preimage
+  """
+  pass
+
+def on_fail_virtual_receive(message):
+  """ Contents: reason?
+  """
+  pass
+
+def on_propose_virtual_send(message):
+  """ Contents: route payment_hash [label] [msatoshi] [bolt11] [partid]
+  """
+  pass
+
+def on_virtual_send_complete(message):
+  """ Contents: payment_preimage
+  """
+  pass
+
+def on_fail_virtual_send(message):
+  """ Contents: reason?
+  """
+  pass
 
 plugin.add_option('greeting', 'Hello', 'The greeting I should use.')
 plugin.run()
